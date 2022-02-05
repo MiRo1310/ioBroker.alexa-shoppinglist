9bc024e3acfba2781e3aae9515080541ebd74139
MiRo1310 <michael.roling@gmx.de>
2022-02-05 12:58:29 +0100

    Initial

---
+++ admin/words.js
@@ -0,0 +1,29 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"alexa-shoppinglist adapter settings": {
+		"en": "Adapter settings for alexa-shoppinglist",
+		"de": "Adaptereinstellungen für alexa-shoppinglist",
+		"ru": "Настройки адаптера для alexa-shoppinglist",
+		"pt": "Configurações do adaptador para alexa-shoppinglist",
+		"nl": "Adapterinstellingen voor alexa-shoppinglist",
+		"fr": "Paramètres d'adaptateur pour alexa-shoppinglist",
+		"it": "Impostazioni dell'adattatore per alexa-shoppinglist",
+		"es": "Ajustes del adaptador para alexa-shoppinglist",
+		"pl": "Ustawienia adaptera dla alexa-shoppinglist",
+		"zh-cn": "alexa-shoppinglist的适配器设置"
+	},
+	"alexastate": {
+		"en": "Alexa State",
+		"de": "Alexa-Zustand",
+		"ru": "Алекса Стэйт",
+		"pt": "Estado Alexa",
+		"nl": "Alexa staat",
+		"fr": "État Alexa",
+		"it": "Stato Alexa",
+		"es": "estado de alexa",
+		"pl": "Stan Alexa",
+		"zh-cn": "亚历克萨州"
+	}
+};
